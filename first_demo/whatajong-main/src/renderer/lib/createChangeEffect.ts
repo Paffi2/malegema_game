86da3784db6cc7f1c17568312434652bf63c86f0
yangbowen <149923886+TensorsT@users.noreply.github.com>
2026-03-19 20:17:14 +0800

    Delete first_demo directory

--- first_demo/whatajong-main/src/renderer/lib/createChangeEffect.ts
+++
@@ -1,8 +0,0 @@
-import { type Accessor, createEffect, on } from "solid-js"
-
-export function createChangeEffect<T>(
-  fn: (prev: T) => void,
-  value: Accessor<T>,
-) {
-  createEffect(on(value, fn, { defer: true }))
-}
